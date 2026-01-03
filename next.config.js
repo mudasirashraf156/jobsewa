7b43dd88dc3c5c7a58f243ab24af85198f9fbdfb
Mudasir Ashraf <87904740+mudasirashraf156@users.noreply.github.com>
2026-01-03 20:01:26 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,38 +1,39 @@
 /** @type {import('next').NextConfig} */
 
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const nextConfig = {
+  reactStrictMode: true,
+
   images: {
     domains: ["api.dicebear.com", "xsgames.co", "res.cloudinary.com"],
   },
-  reactStrictMode: true,
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.join(
-            __dirname,
-            "./node_modules/pdfjs-dist/build/pdf.worker.min.js"
-          ),
-          to: path.join(__dirname, "dist"),
-        },
-      ],
-    }),
-  ],
-  entry: {
-    main: "./src/index.tsx",
-    "pdf.worker": path.join(
-      __dirname,
-      "./node_modules/pdfjs-dist/build/pdf.worker.min.js"
-    ),
+
+  eslint: {
+    ignoreDuringBuilds: true, // IMPORTANT for Vercel
   },
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "[name].bundle.js",
+
+  webpack: (config, { isServer }) => {
+    // Copy pdf worker only for client build
+    if (!isServer) {
+      config.plugins.push(
+        new CopyWebpackPlugin({
+          patterns: [
+            {
+              from: path.join(
+                __dirname,
+                "node_modules/pdfjs-dist/build/pdf.worker.min.js"
+              ),
+              to: path.join(__dirname, "public"),
+            },
+          ],
+        })
+      );
+    }
+
+    return config;
   },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
